91514c002d91b9e6fbd6ca7629b240fc94a9525b
aishwarya shinde <aishwaryagorakhshinde@gmail.com>
2024-01-30 01:13:27 +0530

    add comments

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ function loadMovieDetails(){
         movie.addEventListener('click', async () => {
             // console.log(movie.dataset.id);
             searchList.classList.add('hide-search-list');
-            // movieSearchBox.value = "";
+             movieSearchBox.value = "";
             const result = await fetch(`http://www.omdbapi.com/?i=${movie.dataset.id}&apikey=744e8782`);
             const movieDetails = await result.json();
             // console.log(movieDetails);
